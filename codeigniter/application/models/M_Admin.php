2923211b7125c38e1c0da011898fd974edfe18a7
BagoesIT <59387198+bagoesihsan98@users.noreply.github.com>
2020-03-15 15:07:14 +0700

    Menambahkan fungsi untuk mengamil data dari database

--- codeigniter/application/models/M_Admin.php
+++ codeigniter/application/models/M_Admin.php
@@ -8,6 +8,23 @@
         {
             return $this->db->get('tb_sapi');
         }
+
+        public function tampilUser()
+        {
+            return $this->db->get('tb_user');
+        }
+
+        public function tampilUserTerakhir()
+        {
+            $this->db->order_by('id_user','DESC');
+            return $this->db->get('tb_user',1);
+        }
+
+        public function registerUser($data,$table)
+        {
+            $this->db->insert($table,$data);
+        }
+
     }
 
 ?>
